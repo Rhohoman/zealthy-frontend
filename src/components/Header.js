ec3e901028da7fdd2a41e48582e0e443be505470
Ryan Ho <rhohoman@gmail.com>
2024-12-04 10:29:50 -0500

    css header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,34 @@ import image from "../styles/assets/wellness.png";
 
 export const Header = () => {
   return (
+    // <div className="header">
+    //   <a id="logo" href="/">
+    //     Zealthy
+    //   </a>
+    //   <span className="header-links">
+    //     <a href="/admin">Admins</a>
+    //     <a href="https://github.com/Rhohoman/zealthyproject">Github</a>
+    //     <a href="#">Contact</a>
+    //   </span>
+    //   {/* <img src={image} alt="Wellness-img" /> */}
+    // </div>
     <div className="header">
-      <a id="logo" href="/">
+      <a id="logo" href="/" className="logo">
         Zealthy
       </a>
       <span className="header-links">
-        <a href="/admin">Admins</a>
-        <a href="https://github.com/Rhohoman/zealthyproject">Github</a>
-        <a href="#">Contact</a>
+        <a href="/admin" className="header-link">
+          Admins
+        </a>
+        <a
+          href="https://github.com/Rhohoman/zealthyproject"
+          className="header-link"
+        >
+          Github
+        </a>
+        <a href="#" className="header-link">
+          Contact
+        </a>
       </span>
       {/* <img src={image} alt="Wellness-img" /> */}
     </div>
